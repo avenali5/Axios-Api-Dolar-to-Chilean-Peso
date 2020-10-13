be14ae2c93a74d97cd5cbd81d82ced153fe45199
Alejo <git@github.com>
2020-10-13 14:41:03 -0300

    https://github.com/avenali5/Real-time-Euro-Converter-API.git

--- src/main.js
+++ src/main.js
@@ -3,10 +3,17 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import Vuetify from 'vuetify'
+Vue.use(Vuetify)
+/*import VueAxios from 'vue-axios'
+
+Vue.use(VueAxios, axios)*/
+
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  vuetify : new Vuetify(),
   render: function (h) { return h(App) }
 }).$mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,71 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-layout :wrap="true">
+    <v-flex xs12>
+      <v-card>
+        <h1 v-html="title" class="text-center"></h1>
+      </v-card>
+      <v-card>
+        <v-date-picker 
+          v-model="fecha"
+          full-width
+          locale="es-es"
+          :min="minDate"
+          :max="maxDate"
+          @change="getEuro(fecha)">
+        </v-date-picker>
+      </v-card>
+      <v-card color="error" dark>
+        <v-card-text class="display-1 text-center">
+          {{ value }}
+        </v-card-text>
+      </v-card>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios'
+import {mapMutations}from  'vuex'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      title:'Cambio Peso Chileno - Euro',
+      fecha:new Date().toISOString().substr(0,10),
+      minDate:'1984',
+      maxDate:new Date().toISOString().substr(0,10),
+      value:null
+    }
+  },
+  methods:{
+    ...mapMutations(['mostrarLoading','ocultarLoading']),
+
+
+    async getEuro(day){
+      let ddmmyy = day.split('-').reverse().join('-')
+
+      try{
+        this.mostrarLoading({titulo:'Accediendo a info', color:'secondary'})
+
+        let euroInfo = await axios.get(`https://mindicador.cl/api/euro/${ddmmyy}`)
+        console.log(euroInfo.data.serie)
+        if(euroInfo.data.serie.length > 0){
+          this.value = await euroInfo.data.serie[0].valor
+        } else{
+          this.value = 'Sin resultados'
+        }
+
+      } catch (error){
+
+      }
+      finally{
+        this.ocultarLoading()
+      }
+    },
+  },
+  created(){
+    this.getEuro(this.fecha)
   }
 }
 </script>
